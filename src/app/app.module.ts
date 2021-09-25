d5023d61bedc877bd7b450eb60b5caf033eff8bf
Ruel Baldo <bebebaldo75@gmail.com>
2021-09-25 23:06:03 +0800

    Added new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { MyheaderComponent } from './myheader/myheader.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    MyheaderComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule
